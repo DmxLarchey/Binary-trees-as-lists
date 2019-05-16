f371855e78575c2ca548fc8069146d7bd65710d7
Dominique Larchey-Wendling <larchey@wendling>
2019-05-16 10:03:09 +0200

    completed solutions

--- Students/2019/Lanuel/bt_exos_Lanuel.v
+++ Students/2019/Lanuel/bt_exos_Lanuel.v
@@ -56,9 +56,14 @@ Hint Rewrite length_nil length_cons app_length map_length : length_db.
 
 Fact list_app_inj X (l1 l2 r1 r2 : list X) : length l1 = length l2 -> l1++r1 = l2++r2 -> l1 = l2 /\ r1 = r2.
 Proof.
-  revert l2; induction l1 as [ | x l1 IH ]; intros [ | y l2 ]; try discriminate; simpl.
-  + intros; subst; auto.
-  + intros H1 H2; inversion H2; destruct IH with l2; subst; auto.
+  revert l2; induction l1 as [ | x l1 IH ]; intros [ | y l2 ].
+  intros; split; auto.
+  intros; inversion H.
+  intros; inversion H.
+  intros; injection H0.
+  intros; subst. 
+  case (IH l2); auto; intros; subst; auto.
+ 
 Qed.
 
 Section flat_map.
@@ -67,20 +72,23 @@ Section flat_map.
 
   Fact flat_map_app l1 l2 : flat_map f (l1++l2) = flat_map f l1 ++ flat_map f l2.
   Proof.
-    induction l1; simpl; auto.
-    rewrite app_ass; f_equal; auto. 
+   intros.
+  induction l1.
+  reflexivity.
+  simpl.
+  rewrite IHl1.
+  rewrite app_ass.
+  reflexivity. 
   Qed.
 
   Hypothesis Hf : forall x, f x <> nil.
 
   Fact flat_map_nil l : nil = flat_map f l -> l = nil.
   Proof.
-    destruct l as [ | x l ]; auto.
-    simpl; intros H.
-    symmetry in H.
-    apply app_eq_nil, proj1, Hf in H.
-    destruct H.
-  Qed.
+  intro H.
+  induction l. trivial.
+  destruct H.
+  Admitted.
 
 End flat_map.
 
@@ -166,33 +174,34 @@ Section prefix. (* as an inductive predicate *)
   Fact prefix_app_inv l1 l2 r1 r2 : l1++l2 <p r1++r2 -> { l1 <p r1 } + { r1 <p l1 }.
   Proof.
     revert l2 r1 r2; induction l1 as [ | x l1 IH ]; intros l2 r1 r2.
-    + left; apply in_prefix_0.
-    + destruct r1 as [ | y r1 ].
-      - right; apply in_prefix_0.
-      - simpl; intros H.
-        apply prefix_inv in H.
-        destruct H as [ H1 H ]; subst.
-        destruct IH with (1 := H) as [ H1 | H1 ]; [ left | right ];
-          apply in_prefix_1; trivial.
-  Qed.
+    intros;left;apply in_prefix_0.
+    destruct r1 as [ | y r1 ].
+    intros;right;apply in_prefix_0.
+    intros. apply prefix_inv in H.
+
+    destruct H.
+    subst.
+    left.
+    case(IH l1 l1 l1).
+ apply prefix_app_lft .
+    apply prefix_refl. 
+
+
+  Admitted.
   
 End prefix.
 
 Definition prefix_spec X (l ll : list X) : l <p ll -> { r | ll = l ++ r }.
 Proof.
   induction 1 as [ ll | x l ll _ (r & Hr) ] using prefix_rect.
-  + exists ll; auto.
-  + exists r; subst; auto.
+ exists ll;  auto.
+  exists r; subst;  auto.
 Qed.
 
 Fact prefix_app_lft_inv X (l1 l2 m : list X) : l1++l2 <p m -> { m2 | m = l1++m2 /\ l2 <p m2 }.
 Proof.
-  intros H.
-  destruct prefix_spec with (1 := H) as (m1 & H1).
-  exists (l2++m1); subst.
-  rewrite app_ass; split; auto.
-  apply prefix_right.
-Qed.
+
+Admitted.
 
 (** Binary trees *)
 
@@ -201,12 +210,12 @@ Inductive bt : Set := bt0 | bt1 : bt -> bt -> bt.
 Delimit Scope bt_scope with bt.
 
 Notation null := bt0.    (* typing that is easier *)
-Notation ø := bt0.       (* for a nicer display *)
+Notation u00f8 := bt0.       (* for a nicer display *)
 
 Notation " '<<' x ',' y '>>' " := (bt1 x y) (at level 0): bt_scope.
-Notation " ⟨ x , y ⟩ " := (bt1 x y) (at level 0): bt_scope.
+Notation " \u27e8 x , y \u27e9 " := (bt1 x y) (at level 0): bt_scope.
 
-Reserved Notation "〈 t 〉" (at level 0, no associativity).
+Reserved Notation "\u2329 t \u232a" (at level 0, no associativity).
 Reserved Notation " '[[' t ']]' " (at level 0, no associativity).
 
 Open Scope bt_scope.
@@ -215,47 +224,33 @@ Open Scope bt_scope.
 
 Definition bt_eq_dec (s t : bt) : { s = t } + { s <> t }.
 Proof.
-  revert t; induction s as [ | a Ha b Hb ]; intros [ | c d ];
-    try ((right; discriminate) || (left; auto; fail)).
-  destruct (Ha c) as [ | C ].
-  + destruct (Hb d) as [ | C ]; subst; auto.
-    right; contradict C; inversion C; auto.
-  + right; contradict C; inversion C; auto.
-Qed.
+  revert t; induction s as [ | a Ha b Hb ]; intros [ | c d ].
+Admitted.
 
 Fact bt_pair_neq a1 b1 a2 b2 : <<a1,a2>> <> <<b1,b2>> -> { a1 <> b1 } + { a2 <> b2 }.
 Proof.
-  intros H.
-  destruct (bt_eq_dec a1 b1) as [ | C ].
-  + right; contradict H; subst; auto.
-  + left; auto.
-Qed. 
+Admitted. 
 
 Inductive bt_mirror : bt -> bt -> Prop :=
-  | in_bt_mirror_0 : bt_mirror ø ø
+  | in_bt_mirror_0 : bt_mirror u00f8 u00f8
   | in_bt_mirror_1 : forall a b c d, bt_mirror a b 
                                   -> bt_mirror c d
                                   -> bt_mirror <<a,c>> <<d,b>>.
 
 Definition bt_compute_mirror s : { t | bt_mirror s t }.
 Proof.
-  induction s as [ | a (a'& Ha) b (b' & Hb) ].
-  + exists ø; constructor. 
-  + exists <<b',a'>>; constructor; auto.
-Qed.
+Admitted.
 
 Fixpoint bt_size t :=
   match t with 
-    | ø         => 1
-    | <<t1,t2>> => 1 + 〈 t1 〉 + 〈 t2 〉
+    | u00f8         => 1
+    | <<t1,t2>> => 1 + \u2329 t1 \u232a + \u2329 t2 \u232a
   end
-where "〈 t 〉" := (bt_size t).
+where "\u2329 t \u232a" := (bt_size t).
 
-Fact bt_mirror_size s t : bt_mirror s t -> 〈 s 〉 = 〈 t 〉.
+Fact bt_mirror_size s t : bt_mirror s t -> \u2329 s \u232a = \u2329 t \u232a.
 Proof.
-  induction 1; simpl; f_equal; auto.
-  rewrite plus_comm; f_equal; auto.
-Qed.
+Admitted.
 
 (* Encoding of binary trees as list of Zero and One *)
 
@@ -264,32 +259,40 @@ Notation One  := true.
 
 Fixpoint bt_bin t : list bool :=
   match t with
-    | ø       => Zero :: nil
+    | u00f8       => Zero :: nil
     | <<a,b>> => One :: [[a]] ++ [[b]]
   end
 where "[[ t ]]" := (bt_bin t).
 
-Eval compute in [[ <<ø,ø>> ]].
-Eval compute in [[ <<ø,<<ø,ø>>>> ]].
+Eval compute in [[ <<u00f8,u00f8>> ]].
+Eval compute in [[ <<u00f8,<<u00f8,u00f8>>>> ]].
 
 Fact bt_bin_not_nil t : [[t]] <> nil.
 Proof.
-  destruct t; simpl; discriminate.
-Qed.
+Admitted.
 
 Hint Resolve bt_bin_not_nil.
 
 (* The length of the encoding is the size of the tree *)
 
-Fact bt_bin_length t : length [[t]] = 〈 t 〉.
+Fact bt_bin_length t : length [[t]] = \u2329 t \u232a.
 Proof.
-  induction t as [ | a Ha b Hb ]; simpl; f_equal.
-  rewrite app_length; f_equal; auto.
-Qed.
+induction t;  auto.
+  replace (<< t1, t2 >>) with (1 + < t1 > + < t2 >).
+  simpl;  auto.
+  rewrite app_length.
+  auto.
+  auto.
+Admitted.
 
 Fact bt_bin_length_geq t : 1 <= length [[t]].
 Proof.
-  destruct t; simpl; omega.
+ rewrite bt_bin_length.
+  induction t.
+  constructor.
+  replace ( <t1, t2 > ) with (1 + < t1 > + < t2>).
+  omega.
+  constructor.
 Qed.
 
 (** The essential lemma of non-ambiguity: 
@@ -300,43 +303,26 @@ Qed.
 
 Lemma bt_bin_eq s : forall t l m, [[s]] ++ l = [[t]] ++ m -> s = t /\ l = m. 
 Proof.
-  induction s as [ | s1 IH1 s2 IH2 ]; intros [ | t1 t2 ]; try (intros; discriminate; fail).
-  + intros l m; simpl; inversion 1; subst; auto.
-  + intros l m; simpl; intros H.
-    injection H; clear H; intros H.
-    do 2 rewrite app_ass in H.
-    apply IH1 in H.
-    destruct H as (-> & H).
-    apply IH2 in H.
-    destruct H; subst; auto.
-Qed.
+  induction s as [ | s1 IH1 s2 IH2 ]; intros [ | t1 t2 ].
+Admitted.
 
 Corollary bt_bin_prefix_eq s t : [[s]] <p [[t]] -> s = t.
 Proof.
   intros (l & Hl).
   rewrite (app_nil_end [[t]]) in Hl.
-  apply bt_bin_eq in Hl; firstorder.
-Qed.
+Admitted.
 
 Corollary bt_bin_inj s t : [[s]] = [[t]] -> s = t.
 Proof.
-  intros H; apply bt_bin_prefix_eq; rewrite H.
-  apply prefix_refl.
-Qed.
+Admitted.
 
 Corollary bt_bin_prefix_app_eq s t l m : [[s]]++l <p [[t]]++m -> s = t /\ l <p m.
 Proof.
-  intros (k & Hk); rewrite app_ass in Hk.
-  apply bt_bin_eq in Hk; destruct Hk; subst; split; auto.
-  apply prefix_right.
-Qed.
+Admitted.
 
 Corollary bt_bin_uniq ll t1 t2 : [[t1]] <p ll -> [[t2]] <p ll -> t1 = t2.
 Proof.
-  intros (m1 & H1) (m2 & H2).
-  rewrite H1 in H2.
-  apply bt_bin_eq in H2; tauto.
-Qed.
+Admitted.
 
 Lemma flat_map_bt_bin_eq lt1 : forall lt2 l m, 
      flat_map bt_bin lt1 ++ l = flat_map bt_bin lt2 ++ m
@@ -344,15 +330,7 @@ Lemma flat_map_bt_bin_eq lt1 : forall lt2 l m,
 Proof. 
   induction lt1 as [ | t1 lt1 IH ]; intros lt2 l m E; 
     [ | destruct lt2 as [ | t2 lt2 ] ]; simpl in E.
-  + left; apply in_prefix_0.
-  + right; apply in_prefix_0.
-  + do 2 rewrite app_ass in E.
-    apply bt_bin_eq in E.
-    destruct E as (? & E); subst.
-    apply IH in E.
-    destruct E as [ E | E ]; [ left | right ];
-      apply in_prefix_1; auto.
-Qed.
+Admitted.
 
 (* [[[t1;...;tn]]] = [[t1]]++...++[[tn]] *)
 
@@ -364,31 +342,13 @@ Notation " '[[[' l ']]]' " := (flat_map bt_bin l) (at level 0, no associativity)
 
 *)
 
-Hint Resolve bt_bin_not_nil.
-
 Theorem lbt_bin_inj lt1 lt2 : [[[lt1]]] = [[[lt2]]] -> lt1 = lt2.
 Proof.
   intros H; generalize H; intros H1.
   rewrite (app_nil_end (_ _ lt2)), (app_nil_end (_ _ lt1)) in H.
   apply flat_map_bt_bin_eq in H.
   destruct H as [ (r & H) | (r & H) ]; subst; rewrite flat_map_app in H1.
-  + rewrite (app_nil_end [[[lt1]]]) in H1 at 1.
-    apply app_inv_head, flat_map_nil in H1; auto.
-    subst; rewrite <- app_nil_end; auto.
-  + rewrite (app_nil_end [[[lt2]]]) in H1 at 2.
-    symmetry in H1.
-    apply app_inv_head, flat_map_nil in H1; auto.
-    subst; rewrite <- app_nil_end; auto.
-Qed.
-
-(* Added lemma for the proof of coming bin_bt_dec *)
-
-Lemma bt_prefix_One_inv t l : [[t]] <p One::l -> exists a b, t = <<a,b>> /\ [[a]]++[[b]] <p l.
-Proof.
-  destruct t as [ | a b ]; simpl; intros H;
-    apply prefix_inv in H; destruct H as [ H1 H2 ]; try discriminate.
-  exists a, b; auto.
-Qed.
+Admitted.
 
 (** Now the decoders, beware that bt_bin is injective but not surjective !!
     Indeed, no binary tree encodes into One :: nil for instance *)
@@ -399,26 +359,43 @@ Qed.
      2/ or show that no such tree exists.
 *)
 
+
 Definition bin_bt_dec (lb : list bool) : { t : bt & { r | lb = [[t]] ++ r } } 
                                        + { forall t, ~ [[t]] <p lb }.
 Proof.
   induction on length of lb as [ [ | [] lb1 ] IH ].
-  + right; intros [] (r & Hr); discriminate.
-  + destruct (IH lb1) as [ (t1 & lb2 & H1) | C ].
-    - simpl; omega.
-    - destruct (IH lb2) as [ (t2 & lb' & H2) | C ].
-      * subst; simpl; rewrite app_length; omega.
-      * subst; left; exists <<t1,t2>>, lb'; simpl; rewrite app_ass;auto.
-      * right; intros t Ht. 
-        apply bt_prefix_One_inv in Ht.
-        destruct Ht as (a & b & ? & Ht); subst.
-        apply  bt_bin_prefix_app_eq, proj2, C in Ht; trivial.
-    - right; intros t Ht.
-      apply bt_prefix_One_inv in Ht.
-      destruct Ht as (a & b & ? & Ht); subst.
-      apply (C a), prefix_trans with (2 := Ht), prefix_right. 
-  + left; exists ø, lb1; simpl; auto.
-Qed.
+
+  (* lb = nil *)
+  
+  right; intros [] (r & Hr); discriminate.
+
+  (* lb = One :: lb1  >>>> call the induction hypothesis on lb1 (shorter than lb) *)
+
+  destruct (IH lb1) as [ (t1 & lb2 & H1) | C ]. 
+  admit.
+  
+  (* lb1 = [[t1]] ++ lb2  >>>> call the induction hypothesis on lb2 (shorter than lb1, and thus of lb) *)
+  
+  destruct (IH lb2) as [ (t2 & lb' & H2) | C ].
+  admit.
+
+  (* lb2 = [[t2]] ++ lb' *)
+ 
+  admit.
+   
+  (* ~ [[t]] <p lb2 *)  
+  
+  admit.
+  
+  (* ~ [[t]] <p lb1,  *) 
+  
+  admit. 
+ 
+  (* lb = Zero :: lb1 *)
+  
+  left; exists \u00f8, lb1; simpl; auto.
+
+Admitted.
 
 (* Given a list of boolean values lb, computes a maximal list of trees [t1;...;tk]
    such that lb = [[t1]]++...++[[tk]]++r where r is not prefixed by a bt *)
@@ -428,12 +405,7 @@ Definition bin_lbt_decode (lb : list bool) :
 Proof.
   induction on length of lb as [ lb IH ].
   destruct (bin_bt_dec lb) as [ (t & r & H) | H ].
-  + destruct (IH r) as (lt & m & H1 & H2).
-    * subst; rewrite app_length.
-      generalize (bt_bin_length_geq t); omega.
-    * exists (t::lt), m; subst; simpl; rewrite app_ass; auto.
-  + exists nil, lb; simpl; auto.
-Qed.
+Admitted.
 
 (* lb is not of the form [[t]]++... for any t *)
 
@@ -444,7 +416,7 @@ Proof. intros [] []; discriminate. Qed.
 
 Fact not_prefixed_1 lb : ~ not_prefixed (Zero::lb).
 Proof.
-  intros H; apply (H ø); apply in_prefix_1, in_prefix_0.
+  intros H; apply (H \u00f8); apply in_prefix_1, in_prefix_0.
 Qed.
 
 (* If One::lb is not prefix by a tree then
@@ -457,13 +429,8 @@ Fact not_prefixed_2 lb :
       + { not_prefixed lb }.
 Proof.
   intros H.
-  destruct (bin_bt_dec lb) as [ (t & r & H1) | H1 ]; try tauto.
-  left; exists t, r; split; auto.
-  intros s Hs.
-  apply (H <<t,s>>); simpl.
-  apply in_prefix_1; subst.
-  apply prefix_app_lft; auto.
-Qed.
+  destruct (bin_bt_dec lb) as [ (t & r & H1) | H1 ].
+Admitted.
 
 (** However, every list which is not prefixed by a tree can be extended into (the encoding of) a tree *)
 
@@ -471,25 +438,22 @@ Theorem bin_bt_extend (lb : list bool) : (forall t, ~ [[t]] <p lb) -> { rb : _ &
 Proof.
   fold (not_prefixed lb).
   induction on length of lb as [ [ | [] lb ] IH ]; intros Hlb.
-  * exists [[ø]], ø; auto.
-  * destruct (not_prefixed_2 Hlb) as [ (t & r & H1 & H2) | H1 ].
-    + destruct IH with (2 := H2) as (rb & s & E).
-      - subst; simpl; rew length; omega.
-      - subst; exists rb; simpl; rewrite app_ass, E.
-        exists <<t,s>>; auto.
-    + destruct IH with (2 := H1) as (rb & s & E).
-      - simpl; omega.
-      - exists (rb++Zero::nil), <<s,ø>>; simpl.
-        rewrite <- E, app_ass; auto.
-  * exfalso; revert Hlb; apply not_prefixed_1.
-Qed.
+  
+  admit.
+
+  destruct (not_prefixed_2 Hlb) as [ (t & r & H1 & H2) | H1 ].
+  admit.
+  admit.
+  
+  exfalso; revert Hlb; apply not_prefixed_1.
+Admitted.
 
 (* Any sequence of 0s and 1s is the prefix of some encoded sequence of trees *)
 
 Theorem bin_lbt_complete lb : { lt | lb <p [[[lt]]] }.
 Proof.
   destruct (bin_lbt_decode lb) as (lt & r & H1 & H2).
-  destruct bin_bt_extend with (1 := H2) as (rb & t & H3).
+  destruct (bin_bt_extend H2) as (rb & t & H3).
   exists (lt++t::nil), rb.
   rewrite H1, flat_map_app, app_ass; simpl.
   rewrite <- app_nil_end; f_equal; auto.
